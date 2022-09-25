be21af0c709aa831e3a86c39035a76d7cbadd850
ryo-kihara <work1008.rk@gmail.com>
2022-09-25 12:29:44 +0900

    clean: 不要記述削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import { red } from '@mui/material/colors';
