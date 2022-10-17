a5b4ea6345dc04d53d2971420efb2f0086595f52
ryo-kihara <work1008.rk@gmail.com>
2022-10-17 23:16:13 +0900

    feature: Google Identity Services でのログインに切り替え

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,46 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import { red } from '@mui/material/colors';
+import { useEffect, useRef } from 'react';
+import { Box } from '@mui/system';
 
 const Home: NextPage = () => {
+  const loadScript = (src: string): Promise<void> => {
+    return new Promise<void>((resolve, reject) => {
+      if (document.querySelector(`script[src="${src}"]`)) return resolve()
+      const script = document.createElement('script')
+      script.src = src
+      script.onload = () => resolve()
+      script.onerror = (err) => reject(err)
+      document.body.appendChild(script)
+    })
+  }
+
+  const handleCredentialResponse = (res: google.CredentialResponse): void => {
+    console.log(res.credential);
+  }
+
+  const googleButton = useRef<HTMLDivElement|null>(null)
+
+  useEffect(() => {
+    const src = 'https://accounts.google.com/gsi/client'
+    loadScript(src)
+      .then(() => {
+        if (window.google) {
+          window.google.accounts.id.initialize({
+            client_id: '659259089473-c6noq2r7nc7jf11ujcns3aa0pdntgq0g.apps.googleusercontent.com',
+            callback: handleCredentialResponse
+          })
+          window.google.accounts.id.renderButton(
+            (googleButton.current as unknown as HTMLElement),
+            { theme: 'outline', size: 'large' }
+          )
+        }
+      })
+      .catch(console.error)
+  }, [])
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -19,6 +57,9 @@ const Home: NextPage = () => {
           Welcome to My YouTube!
         </h1>
         <p>You can only watch videos from channels you have subscribed to.</p>
+        <Box sx={{ mt: 3 }}>
+          <div ref={googleButton}></div>
+        </Box>
       </main>
     </div>
   )
